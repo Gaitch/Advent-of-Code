e79022012dc51e23d022a4a48abd5e5b502223d3
Gaitch <28251213+Gaitch@users.noreply.github.com>
2023-12-12 22:01:06 +0100

    implement logic for validating example data

--- 2023/day1/restore.c
+++ 2023/day1/restore.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 
+#define LOWEST 48 // 0
+#define HIGHEST 57 // 9
+
 int main()
 {
-	printf("Hello World\n");
+	// read file
+
+	// store data
+	char testdata[] = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n";
+
+	char first = 'x';
+	char last = 'x';
+	char current = 'x';
+	int sum = 0;
+	// loop through array
+	for(int i = 0; i < sizeof testdata; i++)
+	{
+		current = testdata[i];
+
+		// before each linebreak find first and last value
+		// when line break accourse calcuete sum and reset first and last
+		if(current != '\n')
+		{
+			
+			if(current >= LOWEST && current <= HIGHEST)
+			{
+				//printf("%c\n", current);
+				if(first == 'x')
+				{
+					first = current;
+				}
+				else
+				{
+					last = current;
+				}
+			}
+			if(last == 'x')
+			{
+				last = first;
+			}
+			//printf("first= %c last= %c\n", first, last);
+		}
+		else
+		{
+			printf("%c%c\n", first, last);
+			sum += 10*((int)first-LOWEST) + ((int)last-LOWEST);
+			first = 'x';
+			last = 'x';
+		}
+	}
+	printf("%d \n", sum);
 	return 0;
 }
